3197a0dca74c2cc75313aefed9d0d6ca1e634f54
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-03-05 15:54:50 +0300

    Move files into folders

--- src/LatexTokens.py
+++ src/LatexTokens.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-tokens_df = pd.read_csv("im2latex_formulas.norm.csv")
+tokens_df = pd.read_csv("data/im2latex_formulas.norm.csv")
 
 tokens_list = list(tokens_df["formulas"])
 
